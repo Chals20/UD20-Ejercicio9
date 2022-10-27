0204a47e3a22a70e443e962ee213a70ce8cc17b0
Chals <csanchezbasket@gmail.com>
2022-10-27 12:33:25 +0200

    Commit inicial proyecto Maven

---
+++ ejercicio9.maven/src/main/java/UD20/ejercicio9/maven/App.java
@@ -0,0 +1,13 @@
+package UD20.ejercicio9.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
