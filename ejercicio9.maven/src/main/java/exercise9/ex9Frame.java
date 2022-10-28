70268946e164e5bef8c506d4dda53e3f8db8b690
Oscar Vilamitjana <oscar08850>
2022-10-28 10:00:22 +0200

    Primera version version con imagenes

--- ejercicio9.maven/src/main/java/exercise9/ex9Frame.java
+++ ejercicio9.maven/src/main/java/exercise9/ex9Frame.java
@@ -2,6 +2,8 @@ package exercise9;
 
 import java.awt.EventQueue;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -10,11 +12,16 @@ import javax.swing.JLabel;
 import javax.swing.JToggleButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ex9Frame extends JFrame {
-	
-	boolean esPrimera = true;
 
+	boolean esPrimera = true;
+	int  contador0, contador1, contador2, contador3, contador4, contador5, contador6, contador7;
+
+	ArrayList<String> listaRutas = new ArrayList<>();
 
 	private JPanel contentPane;
 
@@ -36,74 +43,196 @@ public class ex9Frame extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * 
+	 * @throws IOException
 	 */
-	public ex9Frame() {
+	public ex9Frame() throws IOException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 533, 416);
+		setBounds(100, 100, 814, 813);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(4, 4, 0, 0));
-		
-		JToggleButton tglbtnNewToggleButton = new JToggleButton("New toggle button");
-		tglbtnNewToggleButton.addMouseListener(new MouseAdapter() {
+
+		JToggleButton b1 = new JToggleButton();
+		b1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				System.out.println("Hello");
 			}
 		});
-		contentPane.add(tglbtnNewToggleButton);
+		contentPane.add(b1);
+
+		JToggleButton b4 = new JToggleButton();
+		contentPane.add(b4);
+
+		JToggleButton b8 = new JToggleButton();
+		contentPane.add(b8);
+
+		JToggleButton b12 = new JToggleButton();
+		contentPane.add(b12);
+
+		JToggleButton b15 = new JToggleButton();
+		contentPane.add(b15);
+
+		JToggleButton b14 = new JToggleButton();
+		contentPane.add(b14);
+
+		JToggleButton b13 = new JToggleButton();
+		contentPane.add(b13);
+
+		JToggleButton b11 = new JToggleButton();
+		contentPane.add(b11);
+
+		JToggleButton b10 = new JToggleButton();
+		contentPane.add(b10);
+
+		JToggleButton b9 = new JToggleButton();
+		contentPane.add(b9);
+
+		JToggleButton b7 = new JToggleButton();
+		contentPane.add(b7);
+
+		JToggleButton b6 = new JToggleButton();
+		contentPane.add(b6);
+
+		JToggleButton b5 = new JToggleButton();
+		contentPane.add(b5);
+
+		JToggleButton b16 = new JToggleButton();
+		contentPane.add(b16);
+
+		JToggleButton b2 = new JToggleButton();
+		contentPane.add(b2);
+
+		JToggleButton b3 = new JToggleButton();
+		contentPane.add(b3);
+		System.out.println(getClass().getResource("/Oso.png"));
+
+		listaRutas.add("/Oso.png");
+		listaRutas.add("/Leon.png");
+		listaRutas.add("/Conejo.png");
+		listaRutas.add("/Cocodrilo.png");
+		listaRutas.add("/Elefante.png");
+		listaRutas.add("/Mono.png");
+		listaRutas.add("/Obeja.png");
+		listaRutas.add("/Leopardo.png");
 		
-		JToggleButton tglbtnNewToggleButton_4 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_4);
-		
-		JToggleButton tglbtnNewToggleButton_8 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_8);
-		
-		JToggleButton tglbtnNewToggleButton_12 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_12);
-		
-		JToggleButton tglbtnNewToggleButton_15 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_15);
-		
-		JToggleButton tglbtnNewToggleButton_14 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_14);
-		
-		JToggleButton tglbtnNewToggleButton_13 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_13);
-		
-		JToggleButton tglbtnNewToggleButton_11 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_11);
-		
-		JToggleButton tglbtnNewToggleButton_10 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_10);
-		
-		JToggleButton tglbtnNewToggleButton_9 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_9);
-		
-		JToggleButton tglbtnNewToggleButton_7 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_7);
-		
-		JToggleButton tglbtnNewToggleButton_6 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_6);
-		
-		JToggleButton tglbtnNewToggleButton_5 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_5);
-		
-		JToggleButton tglbtnNewToggleButton_1 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_1);
-		
-		JToggleButton tglbtnNewToggleButton_2 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_2);
-		
-		JToggleButton tglbtnNewToggleButton_3 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_3);
-		
-		
-		
-		
-		
+
+	
+		try {
+			b1.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b2.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b3.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b4.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b5.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b6.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b7.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b8.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b9.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b10.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b11.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b12.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b13.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b14.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b15.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+			b16.setIcon(
+					new ImageIcon(ImageIO.read(getClass().getResource(listaRutas.get(ponerRutas(listaRutas))))));
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		// btn.setIcon(new
+		// ImageIcon(ImageIO.read(getClass().getResource("/Blank.png"))));
+		// btn.setSelectedIcon(new
+		// ImageIcon(ImageIO.read(getClass().getResource("/Bomb.png"))));
+	}
+
+	private int ponerRutas(ArrayList<String> listaRutas) {
+		boolean seguir = true;
+		while (seguir) {
+			int numero = dameRandom();
+			switch (numero) {
+			case 1:
+				if (contador1 < 2) {
+					contador1++;
+					return numero;
+				}
+				break;
+			case 2:
+				if (contador2 < 2) {
+					contador2++;
+					return numero;
+				}
+				break;
+			case 3:
+				if (contador3 < 2) {
+					contador3++;
+					return numero;
+				}
+				break;
+
+			case 4:
+				if (contador4 < 2) {
+					contador4++;
+					return numero;
+				}
+				break;
+			case 5:
+				if (contador5 < 2) {
+					contador5++;
+					return numero;
+				}
+				break;
+			case 6:
+				if (contador6 < 2) {
+					contador6++;
+					return numero;
+				}
+				break;
+			case 7:
+				if (contador7 < 2) {
+					contador7++;
+					return numero;
+				}
+				break;
+			case 0:
+				if (contador0 < 2) {
+					contador0++;
+					return numero;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		return 0;
+
+	}
+
+	private int dameRandom() {
+		int min = 0, max = 7;
+		Random random = new Random();
+		int numero = 0;
+		numero = random.nextInt(max - min + 1) + min;
+		return numero;
 	}
 
 }
